3fec6d96cb4dff50dd74bdf339790a6de922e55a
phbits <phbits@outlook.com>
2021-02-26 19:01:18 +0000

    typo

--- Resources/WebsiteFailedLogins.config.psm1
+++ Resources/WebsiteFailedLogins.config.psm1
@@ -817,7 +817,7 @@ Function Assert-ValidIniConfig
 
                             } else {
 
-                                $propertyValue = $lpOutputCsv.$("'$(logField)'")
+                                $propertyValue = $lpOutputCsv.$("'$($logField)'")
 
                                 if ([System.String]::IsNullOrEmpty($propertyValue) -eq $true)
                                 {
